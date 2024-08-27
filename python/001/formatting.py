1b7751bf242bf649e7847248c6f28a52bdbede74
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-26 22:26:01 -0300

    tipagem e formatação

---
+++ python/001/formatting.py
@@ -0,0 +1,18 @@
+#Comentario \o/;
+
+import os;
+os.system('cls');
+
+nome = 'Edson';
+idade = 50;
+peso = 87.9;
+casado = True;
+
+# LINE BREAK -> \n prefix
+
+print(f"""   
+    Nome...: {nome}
+    Idade...: {idade}
+    Peso...: {peso}
+""")
+
---
+++ python/001/tipagem.py
@@ -0,0 +1,9 @@
+x = 5
+
+print(x, type(x))
+
+x = 'Texto'
+
+print(x, type(x))
+
+print(x, type(type(x) == str))

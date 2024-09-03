8ac6186ff65624b27945c07f425948db6dbbff0e
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-02 22:45:08 -0300

    aula002

--- python/002/exercicios.py
+++ python/002/exercicios.py
@@ -51,7 +51,50 @@ def ex6():
 
     print('Próximo múltiplo de 5:', ((n//5) + 1)*5)
 
-ex6()
+
+#07 - PEDREIRO
+hb = 0.2
+lb = 0.4
+
+area_bloco = 0.4*0.2
+
+hm = float(input('Informe a altura: '))
+lm = float(input('Informe a largura: '))
+
+blocos_por_largura = lm/lb
+blocos_por_altura = hm/hb
+print(blocos_por_largura)
+print(blocos_por_altura)
+
+
+AREA_MURO = hm * lm
+# print(f'Gasto com blocos: R$ {GASTO_COM_BLOCOS:.2f}')
+quantidade_de_blocos =  blocos_por_altura*blocos_por_largura
+print(quantidade_de_blocos)
+
+CUSTO_POR_BLOCOS = 5
+CUSTO_MAO_DE_OBRA = 100
+
+BLOCOS_NECESSARIOS = int(((quantidade_de_blocos * 0.05) + quantidade_de_blocos)) + 1
+GASTO_COM_MAO_DE_OBRA = AREA_MURO * CUSTO_MAO_DE_OBRA
+GASTO_COM_BLOCOS = BLOCOS_NECESSARIOS * CUSTO_POR_BLOCOS
+
+GASTO_TOTAL = GASTO_COM_BLOCOS+GASTO_COM_MAO_DE_OBRA
+TOTAL_COM_PIX = GASTO_TOTAL - GASTO_TOTAL*0.05
+TOTAL_COM_CREDITO = GASTO_TOTAL + GASTO_TOTAL*0.03
+
+print('Área total do muro em m²:', AREA_MURO)
+print('Quantidade de blocos necessários:', BLOCOS_NECESSARIOS)
+print(f'Gasto com blocos: R${GASTO_COM_BLOCOS:.2f}' )
+print(f'Gasto com mão de obra: R${GASTO_COM_MAO_DE_OBRA:.2f}')
+print(f"""Gasto total: 
+
+        Valor em dinheiro ou pix: R${TOTAL_COM_PIX:.2f}
+        Valor em débito: R${GASTO_TOTAL:.2f}
+        Valor em crédito: R${TOTAL_COM_CREDITO:.2f}
+
+    """)
+
 
 
 

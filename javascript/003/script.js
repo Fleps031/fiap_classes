d0b95a58befeabfc1d2e0a4ed78ef24123c1f4c9
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-18 22:44:59 -0300

    js003

---
+++ javascript/003/script.js
@@ -0,0 +1,5 @@
+function calculateMedia() {
+    const input1 = document.querySelector('#input1')
+    const input2 = document.querySelector('#input2')
+    console.log((Number(input1.value) + Number(input2.value)) / 2)
+}
